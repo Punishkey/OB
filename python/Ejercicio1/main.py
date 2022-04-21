f5e76c9f84cbc1ae772aed9088632aef6be28067
Punishkey <keyserhh@gmail.com>
2022-04-21 20:04:28 +0200

    Ejercicio1 python

---
+++ python/Ejercicio1/main.py
@@ -0,0 +1,21 @@
+'''
+
+Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+
+Hola, soy [tu_nombre]
+
+Estoy empezando el curso de python
+
+Espero aprender mucho
+
+Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+
+'''
+
+print("Bienvenido")
+
+nombre = input("Introduce tu nombre:")
+
+print("Hola, soy " + nombre)
+print("Estoy empezando el curso de Python")
+print("Espero aprender mucho")
