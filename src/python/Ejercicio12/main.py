73450846b1cc9a895b99ba0bc2ce0b35bc44a3f7
Punishkey <keyserhh@gmail.com>
2022-04-23 15:31:06 +0200

    fix upload exercice 12

--- src/python/Ejercicio12/main.py
+++ src/python/Ejercicio12/main.py
@@ -1,3 +1,13 @@
 '''
 Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
 '''
+
+
+def bisiesto(anio):
+    if anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
+        return "Es bisiesto"
+    else:
+        return "No es bisiesto"
+
+
+anio =print(bisiesto(int(input("Introduce un año: "))))
